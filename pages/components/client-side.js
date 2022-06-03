3e9865cf0e852068af90852fde954baa61380f3e
Harry <harrywebdevtech@gmail.com>
2022-06-03 11:15:09 +0100

    Completed basic Apollo Blog tutorial and fetched/rendered out countries in a grid.

---
+++ pages/components/client-side.js
@@ -0,0 +1,8 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import ClientOnly from "../components/ClientOnly";
+import Countries from "../components/Countries";
+
+export default function ClientSide() {
+  return <div>HI</div>;
+}
